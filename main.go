8e6e304026cca85cc42639e023d157beeb18d66d
psalg-dev <129699997+psalg-dev@users.noreply.github.com>
2025-09-18 20:32:36 +0200

    starting with bigger window

--- main.go
+++ main.go
@@ -21,8 +21,8 @@ func main() {
 	// Create application with options
 	err := wails.Run(&options.App{
 		Title:  "gowails",
-		Width:  1024,
-		Height: 768,
+		Width:  1720,
+		Height: 880,
 		AssetServer: &assetserver.Options{
 			Assets: assets,
 		},
