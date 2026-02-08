e2aceba24e524688bda7dda3938dd47aba307954
psalg-dev <129699997+psalg-dev@users.noreply.github.com>
2026-02-08 05:04:42 +0100

    Agent add08a1d-3217-437d-8a94-1ff1f83366f7: auto-commit (periodic)

--- pkg/app/wails_events_emit_test.go
+++ pkg/app/wails_events_emit_test.go
@@ -6,12 +6,15 @@ import (
 )
 
 func TestEmitEvent_Disabled_NoPanic(t *testing.T) {
+	previous := disableWailsEvents
 	disableWailsEvents = true
-	defer func() { disableWailsEvents = false }()
+	t.Cleanup(func() { disableWailsEvents = previous })
 	emitEvent(context.Background(), "unit:test", map[string]string{"key": "value"})
 }
 
 func TestEmitEvent_NilContext_NoPanic(t *testing.T) {
+	previous := disableWailsEvents
 	disableWailsEvents = false
+	t.Cleanup(func() { disableWailsEvents = previous })
 	emitEvent(nil, "unit:test", nil)
 }
